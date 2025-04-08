fed8ba7576b10477e19bbd272455ccccbe9965f2
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2025-04-07 23:10:29 -0400

    feat(terminal): add JSON serialization for PathSegment
    Introduces a `toJson` method in the `PathSegment` struct to enable conversion of path segment data into a `QJsonObject`. This includes serializing attributes and other fields for easier integration with JSON-based workflows.
    
    Improves code readability by reformatting member declarations and organizing includes.

--- src/terminal/terminal_path_segment.h
+++ src/terminal/terminal_path_segment.h
@@ -1,11 +1,11 @@
 #pragma once
-
+#include "common/common.h"
+#include <QJsonObject>
 #include <QString>
 #include <QVariantMap>
-#include "common/common.h"
 
-namespace TerminalSim {
-
+namespace TerminalSim
+{
 /**
  * @struct PathSegment
  * @brief Represents a segment in a path
@@ -13,14 +13,39 @@ namespace TerminalSim {
  * Defines a single hop between terminals in a path,
  * including mode and weight.
  */
-struct PathSegment {
-    QString from;            ///< Starting terminal name
-    QString to;              ///< Ending terminal name
-    TransportationMode mode; ///< Mode of transport
-    double weight;           ///< Cost of the segment
-    QString fromTerminalId;  ///< ID of start terminal
-    QString toTerminalId;    ///< ID of end terminal
-    QVariantMap attributes;  ///< Additional attributes
-};
+struct PathSegment
+{
+    QString            from;           ///< Starting terminal name
+    QString            to;             ///< Ending terminal name
+    TransportationMode mode;           ///< Mode of transport
+    double             weight;         ///< Cost of the segment
+    QString            fromTerminalId; ///< ID of start terminal
+    QString            toTerminalId;   ///< ID of end terminal
+    QVariantMap        attributes;     ///< Additional attributes
 
+    /**
+     * Converts this PathSegment to a QJsonObject
+     * @return JSON representation of the segment
+     */
+    QJsonObject toJson() const
+    {
+        QJsonObject segmentObj;
+        segmentObj["from"]             = from;
+        segmentObj["to"]               = to;
+        segmentObj["mode"]             = static_cast<int>(mode);
+        segmentObj["weight"]           = weight;
+        segmentObj["from_terminal_id"] = fromTerminalId;
+        segmentObj["to_terminal_id"]   = toTerminalId;
+
+        // Add attributes
+        QJsonObject attrsObj;
+        for (auto it = attributes.begin(); it != attributes.end(); ++it)
+        {
+            attrsObj[it.key()] = QJsonValue::fromVariant(it.value());
+        }
+        segmentObj["attributes"] = attrsObj;
+
+        return segmentObj;
+    }
+};
 } // namespace TerminalSim
